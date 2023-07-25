721b5b42f8efb9d9b6afefe7c5ae5fe9e0b0ccf4
OmarNavarro <omar.n.o.92@gmail.com>
2023-07-25 16:25:53 +0200

    re-speeding-cubes

--- js/cube.js
+++ js/cube.js
@@ -1,46 +1,52 @@
 class Cube {
-  constructor(posX, sizeMultiplier) {
+  constructor(posX, sizeMultiplier, speedMultiplier) {
     this.node = document.createElement("div");
     gameBoxNode.appendChild(this.node);
 
     this.x = posX;
     this.y = 0;
-    this.w = 170;
-    this.h = 120;
+
+    this.w = 170; // Initial width of the cube
+    this.h = 120; // Initial height of the cube
+
     this.gravitySpeed = 1;
     this.isFalling = false;
 
-    this.node.style.width = `${this.w}px`;
-    this.node.style.height = `${this.h}px`;
+    this.w = this.w * sizeMultiplier; // Apply the initial sizeMultiplier
+    this.h = this.h * sizeMultiplier;
+
+    this.speedMultiplier = speedMultiplier;
     this.node.style.position = "absolute";
     this.node.style.top = `${this.y}px`;
     this.node.style.left = `${this.x}px`;
     this.direction = 1;
-    this.isFixed = false
+    this.isFixed = false;
 
-    this.w *= sizeMultiplier;
+    this.w *= sizeMultiplier; // Apply the initial sizeMultiplier
     this.h *= sizeMultiplier;
 
     this.node.style.width = `${this.w}px`;
     this.node.style.height = `${this.h}px`;
   }
 
-  moveRight = () => {
-    if (!this.isFalling) {
-      this.x += 5 * this.direction;
-      this.node.style.left = `${this.x}px`;
 
+
+  moveRight() {
+    if (!this.isFalling) {
+      this.x += 5 * this.direction * this.speedMultiplier; // Apply speedMultiplier here
+      this.node.style.left = `${this.x}px`;
+  
       if (this.x + this.w >= gameBoxNode.offsetWidth) {
         this.x = gameBoxNode.offsetWidth - this.w;
         this.direction = -1;
       }
-
+  
       if (this.x <= 0) {
         this.x = 0;
         this.direction = 1;
       }
     }
-  };
+  }
 
   moveDown = () => {
     if (this.isFalling) {
